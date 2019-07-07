84f7ab0fd597642ff6c3981d26398d1792965d80
Jonas Melo <jonasmelo@ciandt.com>
2019-07-07 01:57:23 -0300

    servico de obter usuario logado

--- UberHack.API/Controllers/WorkNetController.cs
+++ UberHack.API/Controllers/WorkNetController.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using UberHack.API.Contracts;
 using UberHack.API.Entities;
 
@@ -13,15 +11,19 @@ namespace UberHack.API.Controllers
     public class WorkNetController : ControllerBase
     {
         // Mock persona Bianca
-        private int _codigoUsuarioLogado = 1;
-        IBaseRepository<Usuario> _usuarioRepository;
+        private readonly int _codigoUsuarioLogado = 1;
 
-        public WorkNetController(IBaseRepository<Usuario> usuarioRepository)
+        IBaseRepository<Usuario> _usuarioRepository;
+        readonly IBaseRepository<Chat> _chatRepository;
+
+        public WorkNetController(
+            IBaseRepository<Usuario> usuarioRepository,
+            IBaseRepository<Chat> chatRepository)
         {
             _usuarioRepository = usuarioRepository;
+            _chatRepository = chatRepository;
         }
 
-        // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<Usuario>> ObterPossiveisConexoes()
         {
@@ -31,7 +33,19 @@ namespace UberHack.API.Controllers
                 .Where(o => o.FaculdadeId == usuarioLogado.FaculdadeId || o.EmpresaId == usuarioLogado.EmpresaId);
 
             return base.Ok(possiveisConexoes);
-                
+
+        }
+
+        [HttpGet]
+        public Usuario ObterUsuario()
+        {
+            Usuario usuario = _usuarioRepository.Get(_codigoUsuarioLogado);
+
+            usuario.Chats = usuario.Chats.OrderByDescending(o => o.Mensagens.OrderByDescending(m => m.DataHora));
+            foreach (var chat in usuario.Chats)
+                chat.Mensagens = chat.Mensagens.OrderBy(o => o.DataHora);
+
+            return usuario;
         }
 
         private Usuario ObterUsuarioLogado()
